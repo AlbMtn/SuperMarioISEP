afb4a1e5ea2575fecd38229059232590607c83fb
Albane Montanier <160020258+AlbMtn@users.noreply.github.com>
2025-03-19 12:48:30 +0100

    Afficher une image

---
+++ Essai Pygame.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+
+pygame.init() #initialisation
+screen = pygame.display.set_mode((800, 600)) #fenetre
+
+image_path = os.path.join("assets","Mario", "Mario.png")#chemin d'accés sécurisé
+mario = pygame.image.load(image_path) #importation mario
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # Afficher l'image à la position donnée
+    screen.blit(mario, (100, 100))
+
+    # Mettre à jour l'affichage
+    pygame.display.flip()
+
+pygame.quit()
